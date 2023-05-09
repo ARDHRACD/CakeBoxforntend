2687bb522fceffe554e68bec612235bb726a4f00
unknown <ardhradasan@gmail.com>
2023-05-09 12:09:17 +0530

    new one

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms'
+import {Router} from '@angular/router'
+import { CakeService } from '../services/cake.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm=new FormGroup({
+    username:new FormControl("",Validators.required),
+    password:new FormControl("",Validators.required)
+  })
+  get username(){
+    return this.loginForm.get("username")
+  }
+  get password(){
+    return this.loginForm.get("password")
+  }
+  constructor(private router:Router,private service:CakeService){}
+  savelogin(){
+    if(this.loginForm.valid){
+      let formData=this.loginForm.value
+      this.service.getToken(formData).subscribe((res:any)=>localStorage.setItem("token",res.token))
+      
+      
+    }
+  }
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms'
+import {Router} from '@angular/router'
+import { CakeService } from '../services/cake.service';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  regForm=new FormGroup(
+  {
+    username:new FormControl("",Validators.required),
+    email:new FormControl("",Validators.required),
+    password:new FormControl("",Validators.required)
+  }
+  )
+  get username(){
+    return this.regForm.get("username")
+  }
+  get email(){
+    return this.regForm.get("email")
+  }
+  get password(){
+    return this.regForm.get("password")
+  }
+  constructor(private router:Router,private service:CakeService){}
+  createAccount(){
+    if(this.regForm.valid){
+      let formData=this.regForm.value
+      this.service.signUp(formData).subscribe(res=>this.router.navigateByUrl(""))
+    }
+  }
+}
